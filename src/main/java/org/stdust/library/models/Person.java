8195cd5bd86e4d993b1c858e0d677b047618e661
St Dust <donteven@try.it>
2022-08-08 23:03:24 +0200

    OL: Implement basic fields and methods of Person model

--- src/main/java/org/stdust/library/models/Person.java
+++ src/main/java/org/stdust/library/models/Person.java
@@ -1,4 +1,40 @@
 package org.stdust.library.models;
 
 public class Person {
+    private int id;
+
+    //TO DO: Validate this fields in form
+    private String name;
+    private int yearOfBirth;
+
+
+    public Person(int id, String name, int yearOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
 }
