944b9e52555feb72ce0ad6ae4b22d71351352dbe
St Dust <donteven@try.it>
2022-08-09 12:00:19 +0200

    OL: Implement option to get person by referencing their name from index page. Create according view for it to display

--- src/main/java/org/stdust/library/controllers/BooksController.java
+++ src/main/java/org/stdust/library/controllers/BooksController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.stdust.library.dao.BookDAO;
 
@@ -21,5 +22,11 @@ public class BooksController {
     public String index(Model model) {
         model.addAttribute("books", bookDAO.index());
         return "books/index";
+    }
+
+    @GetMapping("/{id}")
+    public String create(Model model, @PathVariable("id") int id) {
+        model.addAttribute("book", bookDAO.findByID(id));
+        return "books/show";
     }
 }
